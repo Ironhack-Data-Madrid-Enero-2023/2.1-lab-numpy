327b140cd8953b5229824e2a169326ebd6b4274d
Pedro Jodar <pedrojodargabarron@gmail.com>
2023-01-23 17:34:30 +0100

    lab resuelto

--- main.py
+++ main.py
@@ -1,72 +1,93 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+np.__version__
+np.show_config
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+a = np.random.random((2, 3, 5))
+print(f'Este metodo genera una matriz con números distribuidos uniformemente entre el 0 y el 1 \nde dimensiones tal y como se representa en el segundo argumento del método: {a}')
 
+#a1 = np.random.randint(10, size = (2, 3, 5))
+#print(f'Este metodo genera una matriz con números aleatorios en el rango que se indica en el primer argumento del método \nde dimensiones tal y como se representa en el segundo argumento del método: {a1}')
 
+#a2 = np.random.rand(2, 3, 5)
+#print(f'Este método es muy similar al que se utilizo para generar a: {a2}')
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5, 2, 3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+a.size == b.size
 
 
 #8. Are you able to add a and b? Why or why not?
-
-
+np.add(a, b)
+# No se puede sumar ya que las formas de las matrices no son compatibles para ser sumadas
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+c = np.transpose(b, (1, 2, 0))
 
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
+d = np.add(a, c)
+print('El método funcionará ya que hemos transpuesto b con las dimensiones que nos interesaba')
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
+print(a)
+print(d)
+print(a.shape)
+print(c.shape)
+print('La diferencia es de +1 en todos los elemenos de d con respecto de a')
 
 #12. Multiply a and c. Assign the result to e.
+print('Volvemos a forzar las dimensiones de c para que se puedan multiplicar')
 
 
+e = np.multiply(a, c)
+print(a)
+print(e)
 
 #13. Does e equal to a? Why or why not?
-
+e == a
+print('Las matrices son iguales')
 
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
-
-
+print(f'el máximo de d es: {d_max}')
+print(f'el mínimo de d es: {d_min}')
+print(f'La media de d es: {d_mean}')
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f = np.empty((2, 3, 5), dtype=float)
 
 
 
 """
-#16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
+#16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 
+# 25 to the corresponding value in f.
 If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
 If a value equals to d_mean, assign 50 to the corresponding value in f.
 Assign 0 to the corresponding value(s) in f for d_min in d.
@@ -74,6 +95,19 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+for i,e in enumerate(d):
+        for j, f in enumerate(e):
+                for k, g in enumerate(f):
+                        if d[i][j][k] > d_min and d[i][j][k] < d_mean:
+                                f[i][j][k] = 25.
+                        elif d[i][j][k] > d_mean and d[i][j][k] < d_max:
+                                f[i][j][k] = 75.
+                        elif d[i][j][k] == d_min:
+                                f[i][j][k] = 0.
+                        elif d[i][j][k] == d_mean:
+                                f[i][j][k] = 50.
+                        elif d[i][j][k] == d_max:
+                                f[i][j][k] = 100.
 
 
 
