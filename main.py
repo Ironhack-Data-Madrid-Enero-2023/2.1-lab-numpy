8d82f4cbb685164e3730df15fa04e6bf3f909e4f
cristina-apostol <crristina.ap8@gmail.com>
2023-01-23 18:00:28 +0100

    final

--- main.py
+++ main.py
@@ -1,69 +1,88 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a = np.random.random((2,3,5))
 
 
 #4. Print a.
+
+print(a)
 
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b=np.ones((5,2,3))
 
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(a.shape)
+print(b.shape)
 
 
 #8. Are you able to add a and b? Why or why not?
 
+#suma=np.array(a)+np.array(b)
 
+#operands could not be broadcast together with shapes (2,3,5) (5,2,3) 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c=b.transpose(1,2,0)
 
+print(c)
+c.shape
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d= a + c
+print("\n\n10:\n" , d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print("\n\n11:\n" , a)
+print("\n\n11:\n", d)
 
-
+#
 
 #12. Multiply a and c. Assign the result to e.
 
-
-
+e= np.multiply(a, c)
+print("\n\n12:\n", e)
 #13. Does e equal to a? Why or why not?
 
 
+if a.all() == e.all():
+        print("Is equal")
 
+else:
+        print("Not equal")
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max=d.max()
+d_min=d.min()
+d_mean=d.mean()
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
 
+f=np.empty((d.shape))
 
+print("\n\n15:\n", f)
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -75,9 +94,9 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+f=np.where(d == d_max , 100 , np.where (d > d_mean, 75 , np.where (d== d_mean , 50 , np.where (d > d_min, 25 , 0 ))))
 
-
-
+print ("\n\n16:\n", f)
 """
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
